652b67c63a44d7a07d2a7c5cf0c0c5ed4799358e
w-t-w <15866369958@163.com>
2023-02-21 16:49:16 +0800

    feat: 添加可模拟的 redux 业务

--- src/index.js
+++ src/index.js
@@ -1,1 +1,128 @@
-console.log('start createStore');
+//redux createStore
+//@param state
+//@param stateChanger
+// const createStore = (state, stateChanger) => {
+//     const getState = () => state;
+//     const dispatch = (action) => stateChanger(state, action);
+//     return {
+//         getState,
+//         dispatch
+//     };
+// };
+// redux reducer
+// const stateChanger = (state, action) => {
+//     const {type, payload} = action;
+//     switch (type) {
+//         case 'UPLOAD_TITLE':
+//             state.title.txt = payload.title;
+//             break;
+//         case 'UPLOAD_CONTENT':
+//             state.content.txt = payload.content;
+//             break;
+//         default:
+//             break;
+//     }
+//     return state;
+// };
+// redux state
+// const appState = {
+//     title: {
+//         txt: '前端这件小事儿'
+//     },
+//     content: {
+//         txt: 'Redux 源码解析'
+//     }
+// };
+// const store = createStore(appState, stateChanger);
+// let state = store.getState();
+// console.log('state:', state.title.txt, state.content.txt);
+// store.dispatch({type: 'UPLOAD_TITLE', payload: {title: '进击的大前端'}});
+// state = store.getState();
+// console.log('state:', state.title.txt, state.content.txt);
+// store.dispatch({type: 'UPLOAD_CONTENT', payload: {content: '手写一个 Promise'}});
+// state = store.getState();
+// console.log('state:', state.title.txt, state.content.txt);
+
+// redux createStore complete
+// const createStore = reducer => {
+//     let state = null;
+//     const listeners = [];
+//     const getState = () => state;
+//     const subscribe = listener => listeners.push(listener);
+//     const dispatch = action => {
+//         state = reducer(state, action);
+//         for (let listener of listeners) {
+//             listener();
+//         }
+//     };
+//     dispatch({});
+//     return {
+//         getState,
+//         subscribe,
+//         dispatch
+//     };
+// };
+// const reducer = (state, action) => {
+//     if (!state) {
+//         return appState;
+//     }
+//     const {type, payload} = action;
+//     switch (type) {
+//         case 'UPLOAD_TITLE':
+//             return {
+//                 ...state,
+//                 title: {
+//                     txt: payload.title
+//                 }
+//             };
+//         case 'UPLOAD_CONTENT':
+//             return {
+//                 ...state,
+//                 content: {
+//                     txt: payload.content
+//                 }
+//             };
+//         default:
+//             return {
+//                 ...state,
+//             };
+//     }
+// };
+// const appState = {
+//     title: {
+//         txt: '前端这点小事儿!'
+//     },
+//     content: {
+//         txt: 'Redux 源码解析'
+//     }
+// };
+// function renderApp(state) {
+//     renderTitle(state);
+//     renderContent(state);
+// }
+// function renderTitle(state) {
+//     let title = document.getElementById('title');
+//     if (!title) {
+//         title = document.createElement('div');
+//         title.id = 'title';
+//         document.body.append(title);
+//         renderTitle(state);
+//     }
+//     title.innerHTML = state.title.txt;
+// }
+// function renderContent(state) {
+//     let content = document.getElementById('content');
+//     if (!content) {
+//         content = document.createElement('div');
+//         content.id = 'content';
+//         document.body.append(content);
+//         renderContent(state);
+//     }
+//     content.innerHTML = state.content.txt;
+// }
+// const store = createStore(reducer);
+// renderApp(appState);
+// store.subscribe(() => renderApp(store.getState()));
+// store.dispatch({type: 'UPLOAD_TITLE', payload: {title: '进击的大前端'}});
+// store.dispatch({type: 'UPLOAD_CONTENT', payload: {content: '手写一个 Promise'}});
+
