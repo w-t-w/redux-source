c5afc480442d111ea589adcd4684170fa4d2cb3b
w-t-w <15866369958@163.com>
2023-02-22 11:45:00 +0800

    accumulation: think of javascript

--- src/index.js
+++ src/index.js
@@ -166,3 +166,84 @@
 // store.dispatch({type: 'UPLOAD_CONTENT', payload: {content: '手写 Promise'}});
 // state = store.getState();
 // console.log('state:', state.title.txt, state.content.txt);
+
+// redux createStore complete white-than-wood
+// const createStore = reducer => {
+//     let state = null;
+//     const listeners = [];
+//     const getState = () => state;
+//     const subscribe = listener => listeners.push(listener);
+//     const dispatch = action => {
+//         state = reducer(state, action);
+//         listeners.forEach(listener => listener());
+//     };
+//     // 初始化
+//     dispatch({});
+//     return {
+//         getState,
+//         subscribe,
+//         dispatch
+//     };
+// };
+// const appState = {
+//     title: {
+//         txt: '前端这件小事儿'
+//     },
+//     content: {
+//         txt: 'Redux 源码解析'
+//     }
+// };
+// const reducer = (state, action) => {
+//     if (!state)
+//         return appState;
+//     const {type, payload} = action;
+//     switch (type) {
+//         case 'UPLOAD_TITLE':
+//             return {
+//                 ...state,
+//                 title: {
+//                     txt: payload.title
+//                 }
+//             };
+//         case 'UPLOAD_CONTENT':
+//             return {
+//                 ...state,
+//                 content: {
+//                     txt: payload.content
+//                 }
+//             };
+//         default:
+//             return {
+//                 ...state
+//             };
+//     }
+// };
+// const renderTitle = state => {
+//     let title = document.getElementById('title');
+//     if (typeof title === 'undefined' || title === null) {
+//         title = document.createElement('div');
+//         title.id = 'title';
+//         document.body.append(title);
+//         renderTitle(state);
+//     }
+//     title.innerHTML = state.title.txt;
+// };
+// const renderContent = state => {
+//     let content = document.getElementById('content');
+//     if (typeof content === 'undefined' || content === null) {
+//         content = document.createElement('div');
+//         content.id = 'content';
+//         document.body.append(content);
+//         renderContent(state);
+//     }
+//     content.innerHTML = state.content.txt;
+// };
+// const renderApp = state => {
+//     renderTitle(state);
+//     renderContent(state);
+// };
+// const store = createStore(reducer);
+// renderApp(store.getState());
+// store.subscribe(() => renderApp(store.getState()));
+// store.dispatch({type: 'UPLOAD_TITLE', payload: {title: '进击的大前端'}});
+// store.dispatch({type: 'UPLOAD_CONTENT', payload: {content: '手写 Promise'}});
