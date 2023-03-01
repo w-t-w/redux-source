7b7adf3426419ce4996e8a64d1c33b20b34facd8
yinwenkai <yinwenkai@exiao.tech>
2023-03-01 17:25:02 +0800

    accumulation:think of redux complete sourceCode

--- src/index.js
+++ src/index.js
@@ -272,3 +272,95 @@
 // }
 
 // redux complete sourceCode
+// const Action_Types = {
+//     INIT: '@@redux/INIT'
+// };
+// function isPlainObject(o) {
+//     if (typeof o !== 'object' || o === null)
+//         return false;
+//     let currentObject = o;
+//     while (Object.getPrototypeOf(currentObject) !== null) {
+//         currentObject = Object.getPrototypeOf(currentObject);
+//     }
+//     return Object.getPrototypeOf(o) === currentObject;
+// }
+// function createStore(reducer, preloadedState, enhancer) {
+//     if (typeof enhancer === 'undefined' && typeof preloadedState === 'function') {
+//         enhancer = preloadedState;
+//         preloadedState = undefined;
+//     }
+//     if (typeof enhancer !== 'undefined') {
+//         if (typeof enhancer !== 'function') {
+//             throw new Error('Expected the enhancer to be a function.');
+//         }
+//         return enhancer(createStore)(reducer, preloadedState);
+//     }
+//     if (typeof reducer !== 'function') {
+//         throw new Error('Expected the reducer to be a function.');
+//     }
+//     let currentReducer = reducer;
+//     let currentState = preloadedState;
+//     let currentListeners = [];
+//     let nextListeners = currentListeners;
+//     let isDispatching = false;
+//
+//     function getState() {
+//         return currentState;
+//     }
+//     function ensureCanMutateNextListeners() {
+//         if (nextListeners !== currentListeners) {
+//             nextListeners = currentListeners.slice();
+//         }
+//     }
+//     function subscribe(listener) {
+//         if (typeof listener !== 'function')
+//             throw new Error('Expected listener to be a function');
+//         let isSubscribe = true;
+//         ensureCanMutateNextListeners();
+//         nextListeners.push(listener);
+//         return function unSubscribe() {
+//             if (!isSubscribe) return;
+//             isSubscribe = false;
+//             ensureCanMutateNextListeners();
+//             const index = nextListeners.indexOf(listener);
+//             nextListeners.splice(index, 1);
+//         }
+//     }
+//     function dispatch(action) {
+//         if (!isPlainObject(action)) {
+//             throw new Error(
+//                 `Actions must be plain objects.
+//                 Use custom middleware for async actions.`
+//             );
+//         }
+//         if (typeof action.type === 'undefined') {
+//             throw new Error(
+//                 `Actions may not have an undefined "type" property.
+//                 Have you misspelled a constant?`
+//             );
+//         }
+//         if (isDispatching) {
+//             throw new Error(
+//                 `Reducers may not dispatch actions.`
+//             );
+//         }
+//         try {
+//             isDispatching = true;
+//             currentState = currentReducer(currentState, action);
+//         } finally {
+//             isDispatching = false;
+//         }
+//         const listeners = currentListeners = nextListeners;
+//         for (let i = 0; i < listeners.length; i++) {
+//             const listener = listeners[i];
+//             listener();
+//         }
+//         return action;
+//     }
+//     dispatch({type: Action_Types.INIT});
+//     return {
+//         getState,
+//         subscribe,
+//         dispatch
+//     };
+// }
